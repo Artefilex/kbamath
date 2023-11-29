71018209e57a774183e943c00bcecfb9911f8eae
Artefilex <baris.tncdmr@gmail.com>
2023-11-30 00:08:56 +0300

    admin contoller bölündü

---
+++ server/controlers/category.js
@@ -0,0 +1,25 @@
+const Category = require("../models/Category");
+const {deleteImageAndDestroyModel} = require("../middleware/delete/deletemodel")
+const {list} = require("../middleware/list")
+const slugField = require("../middleware/slugify");
+
+
+exports.category_list = async (req, res) => {
+    await list(Category,res)
+  };
+  
+  exports.category_add = async (req, res) => {
+    try {
+      const category =  await Category.create({
+        image: req.file.path,
+        title: req.body.title,
+        paramsUrl: slugField(req.body.title),
+      });
+      res.send(`${category} success`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  exports.category_delete = async (req, res) => {
+    await deleteImageAndDestroyModel(Category, req.params.id, res);
+  };
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const quizController = require("../controlers/quiz")
+const educationController = require("../controlers/education")
+const notController = require("../controlers/nots")
+const blogController =require("../controlers/blog")
+const categoryController =require("../controlers/category")
 const adminController = require("../controlers/admin");
 const dotenv = require("dotenv");
 const upload = require("../helpers/singleUpload");
@@ -8,41 +13,41 @@ dotenv.config({ path: "./config.env" });
 
 // BLOG
 
-router.delete("/blogs/delete/:blogid", upload, adminController.blog_delete);
-router.put("/blogs/:blogid", upload, adminController.blog_edit);
-router.get("/blogs/:blogid", adminController.single_blog);
-router.post("/blogs", upload, adminController.blog_create);
-router.get("/blogs", adminController.blog_list);
+router.delete("/blogs/delete/:blogid", upload, blogController.blog_delete);
+router.put("/blogs/:blogid", upload, blogController.blog_edit);
+router.get("/blogs/:blogid", blogController.single_blog);
+router.post("/blogs", upload, blogController.blog_create);
+router.get("/blogs", blogController.blog_list);
 
 
 
 //  Education
-router.delete("/education/:id", adminController.education_delete);
-router.put("/education/:id", upload, adminController.putEducation);
-router.get("/education/:id", adminController.getsingleEducation);
-router.post("/education", upload, adminController.education_create);
-router.get("/education", adminController.education_list);
+router.delete("/education/:id", educationController.education_delete);
+router.put("/education/:id", upload, educationController.putEducation);
+router.get("/education/:id", educationController.getsingleEducation);
+router.post("/education", upload, educationController.education_create);
+router.get("/education", educationController.education_list);
 
 //  Quiz
-router.delete("/quizs/:id", adminController.quiz_delete);
-router.put("/quizs/:id", upload, adminController.edit_quiz);
-router.get("/quizs/:id", adminController.get_single_quiz);
-router.post("/quizs", upload, adminController.create_quiz);
-router.get("/quizs", adminController.quiz_list);
+router.delete("/quizs/:id", quizController.quiz_delete);
+router.put("/quizs/:id", upload, quizController.edit_quiz);
+router.get("/quizs/:id", quizController.get_single_quiz);
+router.post("/quizs", upload, quizController.create_quiz);
+router.get("/quizs", quizController.quiz_list);
 
 // Nots
 
-router.delete("/nots/:id", adminController.nots_delete);
-router.put("/nots/:id", upload, adminController.edit_nots);
-router.get("/nots/:id", adminController.get_single_nots);
-router.post("/nots", upload, adminController.create_nots);
-router.get("/nots", adminController.nots_list);
+router.delete("/nots/:id", notController.nots_delete);
+router.put("/nots/:id", upload, notController.edit_nots);
+router.get("/nots/:id", notController.get_single_nots);
+router.post("/nots", upload, notController.create_nots);
+router.get("/nots", notController.nots_list);
 
 // Category
 
-router.delete("/category/:id", adminController.category_delete);
-router.post("/category", upload, adminController.category_add);
-router.get("/category", adminController.category_list);
+router.delete("/category/:id", categoryController.category_delete);
+router.post("/category", upload, categoryController.category_add);
+router.get("/category", categoryController.category_list);
 
 router.delete("/users/:id", adminController.user_delete);
 router.put("/users/:id", upload, adminController.edit_users);
