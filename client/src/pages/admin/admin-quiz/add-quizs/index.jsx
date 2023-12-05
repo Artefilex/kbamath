b35aa20a2e9a6becae4803f9d69ee9fe9bb87e30
Artefilex <baris.tncdmr@gmail.com>
2023-12-06 02:16:25 +0300

    add-quiz ve edit-quiz de useFormik ve yup kullanıldı

--- client/src/pages/admin/admin-quiz/add-quizs/index.jsx
+++ client/src/pages/admin/admin-quiz/add-quizs/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 import {
   FormContent,
@@ -6,67 +6,91 @@ import {
   FormButton,
 } from "../../../../components/form";
 import { addItem } from "../../../../servises/admin";
+import {useFormik} from "formik"
+import { QuizShema } from "../../validations/quizShema";
 export default function AddQuizs() {
   const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues:{
+      image:"",
+      title:"",
+      iframeUrl:"",
+      iframeHeight:"",
+    },
+    validationSchema: QuizShema,
+    onSubmit: async (values) =>{
+      const formData = new FormData();
+      formData.append("image", values.image);
+      formData.append("title",  values.title);
+      formData.append("iframeUrl",  values.iframeUrl);
+      formData.append("iframeHeight",  values.iframeHeight);
+      const addQuizs = async () => {
+        await addItem("quizs" ,formData , "Quizs")
+  
+      };
+      await addQuizs();
+      navigate("/admin/quizs");
+    }
 
-  const [image, setImage] = useState("");
-  const [title, setTitle] = useState("");
-  const [iframeUrl, setIframeUrl] = useState("");
-  const [ iframeHeight, setIframeHeight] = useState("");
+  })
 
-  const handleSubit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("image", image);
-    formData.append("title", title);
-    formData.append("iframeUrl", iframeUrl);
-    formData.append("iframeHeight", iframeHeight);
-    const addQuizs = async () => {
-      await addItem("quizs" ,formData , "Quizs")
-
-    };
-    await addQuizs();
-    navigate("/admin/quizs");
-  };
   return (
         <form
           encType="multipart/form-data"
-          onSubmit={handleSubit}
+          onSubmit={formik.handleSubmit}
           method="POST"
           className="w-full rounded-xl py-4 flex-col flex items-center justify-center  gap-3"
         >
           <FormContent header={"Sınav Başlığı"}>
             <FormInput
+              id="title"
               type="text"
               name="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
+              value={formik.values.title}
+              onChange={formik.handleChange}
+              formikError={formik.touched.title && Boolean(formik.errors.title)}
+              helperText={formik.touched.title && formik.errors.title}
+              handleBlur={formik.handleBlur}
             />
           </FormContent>
 
           <FormContent header={"Sınav Linki"}>
             <FormInput
+              id="iframeUrl"
               type="text"
               name="iframeUrl"
-              value={iframeUrl}
-              onChange={(e) => setIframeUrl(e.target.value)}
+              value={formik.values.iframeUrl}
+              onChange={formik.handleChange}
+              formikError={formik.touched.iframeUrl && Boolean(formik.errors.iframeUrl)}
+          helperText={formik.touched.iframeUrl && formik.errors.iframeUrl}
+          handleBlur={formik.handleBlur}
             />
           </FormContent>
 
           <FormContent header={"Form Yüksekliği"}>
           <FormInput
-              type="number"
+            id="iframeHeight"
+              type="text"
               name="iframeHeight"
-              value={iframeHeight}
-              onChange={(e) => setIframeHeight(e.target.value)}
+              value={formik.values.iframeHeight}
+              onChange={formik.handleChange}
+              formikError={formik.touched.iframeHeight && Boolean(formik.errors.iframeHeight)}
+              helperText={formik.touched.iframeHeight && formik.errors.iframeHeight}
+              handleBlur={formik.handleBlur}
             />
           </FormContent>
 
           <FormContent header={"Başlık Fotoğrafı"}>
             <FormInput
+              id="image"
               type="file"
               name="image"
-              onChange={(e) => setImage(e.target.files[0])}
+              onChange={(e) =>
+                formik.setFieldValue("image", e?.currentTarget?.files[0])
+              }
+              formikError={formik.touched.image && Boolean(formik.errors.image)}
+              helperText={formik.touched.image && formik.errors.image}
+              handleBlur={formik.handleBlur}
             />
           </FormContent>
           <FormButton> Sınav Ekle </FormButton>
--- client/src/pages/admin/admin-quiz/edit-quizs/index.jsx
+++ client/src/pages/admin/admin-quiz/edit-quizs/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   FormContent,
@@ -6,81 +6,109 @@ import {
   FormButton,
 } from "../../../../components/form";
 import { getSingleItem, editItem } from "../../../../servises/admin";
+import {useFormik} from "formik"
+import { EditQuizShema } from "../../validations/quizShema";
 export default function EditQuizs() {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [image, setImage] = useState("");
-  const [title, setTitle] = useState("");
-  const [iframeUrl, setIframeUrl] = useState("");
-  const [ iframeHeight, setIframeHeight] = useState("");
-  const [oldImage, setOldImage] = useState("");
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await  getSingleItem(`quizs/${id}`)
-      setTitle(data.title);
-      setIframeUrl(data.iframeUrl);
-      setIframeHeight(data.iframeHeight);
-      setOldImage(data.image);
-    };
-    fetchData();
-  }, [id]);
 
-  const handleSubit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("oldImage", oldImage);
-    formData.append("image", image);
-    formData.append("title", title);
-    formData.append("iframeUrl", iframeUrl);
-    formData.append("iframeHeight", iframeHeight);
+  const formik = useFormik({
+    initialValues:{
+      image:"",
+      title:"",
+      iframeUrl:"",
+      iframeHeight:"",
+      oldImage:""
+    },
+    validationSchema: EditQuizShema,
+    onSubmit: async (values) =>{
+      const formData = new FormData();
+      formData.append("image", values.image);
+      formData.append("oldImage", values.oldImage);
+      formData.append("title",  values.title);
+      formData.append("iframeUrl",  values.iframeUrl);
+      formData.append("iframeHeight",  values.iframeHeight);
     const editQuizs = async () => {
        const message = "quizs"
-       await editItem(`quizs/${id}` ,formData , message , title) 
+       await editItem(`quizs/${id}` ,formData , message , values.title) 
        navigate("/admin/quizs");
     };
      editQuizs();
-   
-  };
+    }
+
+  }) 
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await  getSingleItem(`quizs/${id}`)
+      formik.setValues({
+        oldImage:data.image,
+      title:data.title,
+      iframeUrl:data.iframeUrl,
+      iframeHeight:data.iframeHeight,
+      })
+  
+    };
+    fetchData();
+  }, [id]);
+ 
   return (
         <form
           encType="multipart/form-data"
-          onSubmit={handleSubit}
+          onSubmit={formik.handleSubmit}
           method="POST"
           className="w-full rounded-xl py-4 flex-col flex items-center justify-center  gap-3"
         >
-       <input type="hidden" name="oldImage" value={oldImage} />
+       <input type="hidden" name="oldImage" value={formik.values.oldImage} />
        <FormContent header={"Sınav Başlığını  Güncelle"}>
-            <FormInput
+       <FormInput
+              id="title"
               type="text"
               name="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
+              value={formik.values.title}
+              onChange={formik.handleChange}
+              formikError={formik.touched.title && Boolean(formik.errors.title)}
+              helperText={formik.touched.title && formik.errors.title}
+              handleBlur={formik.handleBlur}
             />
           </FormContent>
 
           <FormContent header={"Sınav Linkini Güncelle"}>
-            <FormInput
+          <FormInput
+              id="iframeUrl"
               type="text"
               name="iframeUrl"
-              value={iframeUrl}
-              onChange={(e) => setIframeUrl(e.target.value)}
+              value={formik.values.iframeUrl}
+              onChange={formik.handleChange}
+              formikError={formik.touched.iframeUrl && Boolean(formik.errors.iframeUrl)}
+          helperText={formik.touched.iframeUrl && formik.errors.iframeUrl}
+          handleBlur={formik.handleBlur}
             />
           </FormContent>
 
           <FormContent header={"Form Yüksekliğini Güncelle"}>
           <FormInput
-              type="number"
+            id="iframeHeight"
+              type="text"
               name="iframeHeight"
-              value={iframeHeight}
-              onChange={(e) => setIframeHeight(e.target.value)}
+              value={formik.values.iframeHeight}
+              onChange={formik.handleChange}
+              formikError={formik.touched.iframeHeight && Boolean(formik.errors.iframeHeight)}
+              helperText={formik.touched.iframeHeight && formik.errors.iframeHeight}
+              handleBlur={formik.handleBlur}
             />
           </FormContent>
 
           <FormContent header={"Başlık Fotoğrafını Güncelle"}>
-            <FormInput
+          <FormInput
+              id="image"
               type="file"
               name="image"
-              onChange={(e) => setImage(e.target.files[0])}
+              onChange={(e) =>
+                formik.setFieldValue("image", e?.currentTarget?.files[0])
+              }
+              formikError={formik.touched.image && Boolean(formik.errors.image)}
+              helperText={formik.touched.image && formik.errors.image}
+              handleBlur={formik.handleBlur}
             />
           </FormContent>
 
