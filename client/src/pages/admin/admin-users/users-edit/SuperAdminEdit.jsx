f824d9759758f3b44eac2e23b7477c29f9b4fa82
Artefilex <baris.tncdmr@gmail.com>
2023-12-11 19:24:22 +0300

    admin list düzenlendi

--- client/src/pages/admin/admin-users/users-edit/SuperAdminEdit.jsx
+++ client/src/pages/admin/admin-users/users-edit/SuperAdminEdit.jsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { editUserForSuperAdmin } from "../../../../servises/admin"; 
+import { useNavigate } from "react-router-dom";
+import { editUserForSuperAdmin, getAllItems} from "../../../../servises/admin";
 import { FormContent, FormButton } from "../../../../components/form";
+import PropTypes from "prop-types"
+function SuperAdminEdit({editid}) {
+  const [makeAdminBySuper, setMakeAdminBySuper] = useState(false);
 
-function SuperAdminEdit() {
-  const [makeAdminBySuper, setMakeAdminBySuper] = useState(false); 
-  const { editid } = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await getAllItems("users");
+      const filterUser = response.find((user) => user.username === editid);
+      setMakeAdminBySuper(filterUser.isAdmin);
+    };
+    fetchUsers();
+  }, [editid]);
 
   const userMakeAdmin = async (e) => {
     e.preventDefault();
@@ -13,21 +22,38 @@ function SuperAdminEdit() {
     formData.append("isAdmin", makeAdminBySuper);
     console.log(editid, formData);
     await editUserForSuperAdmin(editid, formData);
+     navigate("/admin/users");
   };
 
+  
   return (
-    <form onSubmit={userMakeAdmin}>
+    <form
+      onSubmit={userMakeAdmin}
+      className="w-full rounded-xl py-4 flex-col flex items-center justify-center  gap-3 relative"
+    >
       <FormContent>
-        <input
-          type="checkbox"
-          checked={makeAdminBySuper} 
-          onChange={(e) => setMakeAdminBySuper(e.target.checked)} 
-        />
+        <div className="flex items-center justify-center gap-4">
+          <input
+            className="px-5 py-5 text-[50px]"
+            type="checkbox"
+            checked={makeAdminBySuper}
+            onChange={(e) => setMakeAdminBySuper(e.target.checked)}
+          />
+          {makeAdminBySuper ? (
+            <div>Admin Yetkisi Verildi</div>
+          ) : (
+            <div>Admin değil </div>
+          )}
+        </div>
       </FormContent>
-
-      <FormButton>Admin Yap</FormButton>
+      <FormButton>Kullanıcıyı güncelle</FormButton> 
+     
+      
     </form>
   );
+}
+SuperAdminEdit.propTypes = {
+    editid: PropTypes.string
 }
 
 export default SuperAdminEdit;
--- client/src/pages/admin/admin-users/users-list/index.jsx
+++ client/src/pages/admin/admin-users/users-list/index.jsx
@@ -1,20 +1,20 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import {  getAllItems, handleDelete } from "../../../../servises/admin";
+import { getAllItems, handleDelete } from "../../../../servises/admin";
 import { Tooltip } from "react-tooltip";
 import SuperAdmin from "../../../../helpers/is-super-admin";
-
+import SuperAdminEdit from "../users-edit/SuperAdminEdit";
+import { IoClose } from "react-icons/io5";
 export default function UsersList() {
   const [users, setUsers] = useState([]);
   const [loginUser, setLoginUser] = useState(null);
-
-   useEffect(() => {
+  const [showModal, setShowModal] = useState(false);
+  useEffect(() => {
     const fetchUsers = async () => {
       const getLogin = await JSON.parse(localStorage.getItem("userLogin"));
       setLoginUser(getLogin);
       const response = await getAllItems("users");
-    setUsers(response)
-    
+      setUsers(response);
     };
     fetchUsers();
   }, []);
@@ -31,81 +31,116 @@ export default function UsersList() {
     }
   };
 
-
-  const {filtered} = SuperAdmin()
+  const { filtered } = SuperAdmin();
   return (
     <div className="w-[95%] flex flex-col gap-2">
-      User List
-      { filtered &&  users.map((user) => (
+      <div className="w-full flex items-center justify-between px-4 py-2 bg-slate-700 text-white rounded-t-lg font-bold  "> <div> Kullanıcı Adı</div> <div>Email</div> <div> Aksiyon</div> </div>
+      {filtered &&
+        users.map((user) => (
           <div
             key={user.id}
             className="w-full flex items-center justify-between flex-col border rounded-lg px-2 py-4 mobile:flex-row bg-[color:var(--bg-secondary)] gap-4 mobile:gap-0 "
           >
-            <div className="flex items-center justify-around gap-4 w-full">
-              <div>{user.username}</div>
-              <div>{user.email}</div>
+            <div className="flex items-center justify-around gap-4 w-full flex-col mobile:flex-row">
+              <div className="w-full flex items-center gap-2 ">  
+              <span className="block mobile:hidden font-bold">username:</span>
+              {user.username}</div>
+              <div className="w-full flex items-center  gap-2 "> 
+              <span className="block mobile:hidden font-bold">Email:</span>
+              {user.email}</div>
             </div>
             <div>
               <Tooltip
-                content="Admin Yetkisi var sadece editleyebilirsin"
+                content="Sadece Güncelleyebilirsin"
                 anchorSelect="#admin"
               />
               {user.superAdmin ? (
-                <Link
-                  to={`/admin/users/${user.paramsUrl}`}
+                <div className="w-full flex items-center justify-between ">
+                  <Link
+                    to={`/admin/users/${user.paramsUrl}`}
+                    className=" px-4 py-2 transition-color duration-300 hover:rounded-lg hover:text-white  "
+                    id="admin"
+                  >
+                    Edit
+                  </Link>
+                </div>
+              ) : (
+                <div className="flex items-center justify-between relative">
+                  {/* {
+                user.isAdmin ? (  <Link
+                  to={`/admin/edit-users/${user.paramsUrl}`}
                   className=" px-4 py-2 transition-color duration-300 hover:rounded-lg hover:text-white  "
-                  id="admin"
+                 
                 >
-                  Edit
-                </Link>
-              ):(
-              <div className="flex items-center justify-between ">
-                <Link
+                  Admin
+                </Link>) : (  <Link
                   to={`/admin/edit-users/${user.paramsUrl}`}
                   className=" px-4 py-2 transition-color duration-300 hover:rounded-lg hover:text-white  "
                  
                 >
                   Admin Yap
-                </Link>
-              <button
-                  className="hover:bg-red-700 px-4 py-2 transition-color duration-300 hover:rounded-lg hover:text-white  "
-                  onClick={() => deleteUser(user.paramsUrl)}
-                >
-                  Delete
-                </button>
-              </div>
-              )  }
+                </Link>)
+              } */}
+                  <button
+                    className="mr-4"
+                    onClick={() => setShowModal(!showModal)}
+                  >
+                    Edit
+                  </button>
+                  {showModal && (
+                    <div className="absolute w-full min-h-[6rem] min-w-[12rem] mobile:min-w-[18rem] flex flex-col top-8 -right-20  mobile:top-0  mobile:right-[100px] bg-black border border-slate-800 z-50 ">
+                      <button
+                        className="flex flex-col h-[30px] items-center justify-center z-[6] rounded-bl-lg w-[30px] absolute bg-slate-800 text-white right-0 top-0"
+                        onClick={() => setShowModal(false)}
+                      >
+                        <IoClose className="h-[2rem]" />
+                      </button>
+
+                      <div className="flex items-center flex-col mt-4 justify-center">
+                        <SuperAdminEdit editid={user.paramsUrl} />
+
+                        <button
+                          className="hover:bg-red-700 px-4 py-2 transition-color duration-300 hover:rounded-lg hover:text-white "
+                          onClick={() => deleteUser(user.paramsUrl)}
+                        >
+                          Kullanıcıyı Sil
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              )}
             </div>
           </div>
         ))}
-      {
-        !filtered && users.filter((user) => user.email === loginUser.email).map((singleUser) =>(
-          <div
-          key={singleUser.id}
-          className="w-full flex items-center justify-between flex-col border rounded-lg px-2 py-4 mobile:flex-row bg-[color:var(--bg-secondary)] gap-4 mobile:gap-0 "
-        >
-          <div className="flex items-center justify-around gap-4 w-full">
-            <div>{singleUser.username}</div>
-            <div>{singleUser.email}</div>
-          </div>
-          <div>
-            <Tooltip
-              content="Admin Yetkisi var sadece editleyebilirsin"
-              anchorSelect="#admin"
-            />
-          
-              <Link
-                to={`/admin/users/${singleUser.paramsUrl}`}
-                className=" px-4 py-2 transition-color duration-300 hover:rounded-lg hover:text-white  "
-                id="admin"
-              >
-                Edit
-              </Link>
-           
-          </div>
-        </div>
-        ))
-      }  
+      {!filtered &&
+        users
+          .filter((user) => user.email === loginUser.email)
+          .map((singleUser) => (
+            <div
+              key={singleUser.id}
+              className="w-full flex items-center justify-between flex-col border rounded-lg px-2 py-4 mobile:flex-row bg-[color:var(--bg-secondary)] gap-4 mobile:gap-0 "
+            >
+              <div className="flex items-center justify-around gap-4 w-full">
+                <div>{singleUser.username}</div>
+                <div>{singleUser.email}</div>
+              </div>
+              <div>
+                <Tooltip
+                  content="Admin Yetkisi var sadece editleyebilirsin"
+                  anchorSelect="#admin"
+                />
+
+                <Link
+                  to={`/admin/users/${singleUser.paramsUrl}`}
+                  className=" px-4 py-2 transition-color duration-300 hover:rounded-lg hover:text-white  "
+                  id="admin"
+                >
+                  Edit
+                </Link>
+              </div>
+            </div>
+          ))}
     </div>
   );
 }
