de8732314152b3b9ac5b88f5c939b5ea52f8a6d8
Artefilex <baris.tncdmr@gmail.com>
2023-12-14 06:30:17 +0300

    register için istek düzenlendi db sıfırlandı

--- client/src/servises/admin.js
+++ client/src/servises/admin.js
@@ -1,16 +1,14 @@
-import axios from 'axios';
-import toast from 'react-hot-toast';
-
+import axios from "axios";
+import toast from "react-hot-toast";
 
 // const baseUrl = "http://localhost:4000/admin"
-const baseUrl = "https://sisterslab.onrender.com/admin"
-export  const handleDelete = async ( url,successMessage, errorMessage) => {
+const baseUrl = "https://sisterslab.onrender.com/admin";
+export const handleDelete = async (url, successMessage, errorMessage) => {
   const confirms = window.confirm("Silmek istediğine emin misin");
 
   if (confirms) {
     try {
-     await axios.delete(`${baseUrl}/${url}`, 
-    );
+      await axios.delete(`${baseUrl}/${url}`);
       toast.success(successMessage);
     } catch (error) {
       console.error("Error deleting item:", error);
@@ -18,115 +16,120 @@ export  const handleDelete = async ( url,successMessage, errorMessage) => {
     }
   } else {
     toast.error("İşlem iptal edildi");
-  } 
- return confirms
+  }
+  return confirms;
 };
 
-export const getAllItems = async(item) => {
-    const {data} = await axios.get(`${baseUrl}/${item}`)
+export const getAllItems = async (item) => {
+  const { data } = await axios.get(`${baseUrl}/${item}`);
 
-    return data
-}
+  return data;
+};
 
-export const getSingleItem = async(item) => {
-    const {data} = await axios.get(`${baseUrl}/${item}`)
-    return data
-}
+export const getSingleItem = async (item) => {
+  const { data } = await axios.get(`${baseUrl}/${item}`);
+  return data;
+};
 
-export const editItem = async(id, formdata , message ,title)  => {
-    const response = await axios.put(`${baseUrl}/${id}` ,formdata, {
+export const editItem = async (id, formdata, message, title) => {
+  const response = await axios.put(`${baseUrl}/${id}`, formdata, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  if (response.status === 200) {
+    toast.success(`${title} ${message} güncellendi `);
+  } else {
+    toast.error(`${message} güncellenemedi`);
+  }
+  return response;
+};
+
+export const addItem = async (url, formData, title) => {
+  const response = await axios.post(`${baseUrl}/${url}`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  if (response.status === 200) {
+    toast.success(`${title} Eklendi`);
+  } else {
+    toast.error(`${title}  Eklenemedi`);
+  }
+};
+export const addClass = async (title, message) => {
+  const response = await axios.post(
+    `${baseUrl}/class`,
+    {
+      title: title,
+    },
+    {
       headers: {
-       'Content-Type': 'multipart/form-data',
-      }})
-    if (response.status === 200) {
-        toast.success(`${title} ${message} güncellendi `);
-      } else {
-        toast.error( `${message} güncellenemedi`);
-      }
-    return response
-   
-}
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
-export const addItem = async( url, formData, title ) =>{
- 
-    const response = await axios.post(`${baseUrl}/${url}`,
-        formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },}
-        
-      );
-      
-      if (response.status === 200) {
-        toast.success(`${title} Eklendi`);
-      } else {
-        toast.error( `${title}  Eklenemedi`);
-      }
-    };
-    export const addClass = async( title, message ) =>{
- 
-      const response = await axios.post(`${baseUrl}/class`,
+  if (response.status === 200) {
+    toast.success(`${message} Eklendi`);
+  } else {
+    toast.error(`${message}  Eklenemedi`);
+  }
+};
+
+export const userLogin = async (formData) => {
+  try {
+    const response = await axios.post(`${baseUrl}/login`, formData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response.data);
+    if (response.data.isAdmin) {
+      await localStorage.setItem("userLogin", JSON.stringify(response.data));
+    }
+    return response.data;
+  } catch (error) {
+    console.error("Error during login:", error);
+  }
+};
+
+export const editUserForSuperAdmin = async (id, formData) => {
+  try {
+    console.log(id, formData);
+    const response = await axios.put(
+      `${baseUrl}/users/make-admin/${id}`,
+      formData,
       {
-        title: title,
-      },{
-            headers: {
-              'Content-Type': 'application/json',
-            },});
-        
-        if (response.status === 200) {
-          toast.success(`${message} Eklendi`);
-        } else {
-          toast.error( `${message}  Eklenemedi`);
-        }
-      };
-
-    export const userLogin = async (formData) => {
-      try {
-        const response = await axios.post(`${baseUrl}/login`, formData, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-        console.log(response.data) 
-        if(response.data.isAdmin){
-          await localStorage.setItem("userLogin", JSON.stringify(response.data)) 
-        }
-        return response.data
-      } catch (error) {
-        console.error('Error during login:', error);  
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-        
-    };
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error during login:", error);
+  }
+};
 
-    export const editUserForSuperAdmin = async (id, formData) => {
-      try {
-        console.log(id , formData)
-        const response = await axios.put(`${baseUrl}/users/make-admin/${id}`, formData, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-        console.log(response.data)
-        return response.data
-      } catch (error) {
-        console.error('Error during login:', error);  
-      }
-        
-    };
+export const addUser = async (formData, title) => {
+  try {
+    const response = await axios.post(`${baseUrl}/users`, formData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-    export const addUser  = async( formData, title ) =>{
- 
-      const response = await axios.post(`${baseUrl}/users`,
-          formData, {
-            headers: {
-              'Content-Type': 'application/json',
-            },}
-          
-        );
-        
-        if (response.status === 200) {
-          toast.success(`${title} Eklendi`);
-        } else {
-          toast.error( `${title}  Eklenemedi`);
-        }
-      };
+    console.log(response.data);
+
+    if (response.status === 200) {
+      toast.success(`${title} Eklendi`);
+    } else {
+      toast.error(`${title}  Eklenemedi`);
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+  }
+};
